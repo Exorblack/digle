4384fce128accf705a1c5e3c5f55bb9c3b467c86
Mohamed Hassan <exorblack@gmail.com>
2024-07-07 22:53:47 +0300

    edited

--- src/components/compos/Home/Nav.tsx
+++ src/components/compos/Home/Nav.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { FaInstagram , FaDribbble } from "react-icons/fa";
 import { AiFillTikTok } from "react-icons/ai";
 import { FaBehance } from "react-icons/fa6";
+import Link from "next/link";
+import { Briefcase, Contact } from "lucide-react";
 
 
 interface Navo{
@@ -15,7 +17,7 @@ const nav:React.FC<Navo> = ({id}) => {
     <div className= "border-b-2 p-4" id={id}>
        <div className="flex justify-between items-center">
       <ul className=" flex justify-center gap-6 md:gap-8">
-    <li>
+        <li>
           <a href="https://www.instagram.com/digle_agency" target="_blank" rel="noopener noreferrer" className="text-neutral-700 transition hover:text-neutral-700/75 dark:text-neutral-400 dark:hover:text-neutral-200">
             <FaInstagram className='text-2xl' />
             <span className="sr-only">Instagram</span>
@@ -36,7 +38,24 @@ const nav:React.FC<Navo> = ({id}) => {
           </a>
         </li>
     </ul>
-    {/* <div className="font-bold text-green-600">contact@digle.org</div> */}
+      <div>
+      <ul className=" flex justify-center gap-6 md:gap-8">
+
+        <li>
+          <Link href="/our-services"className="p-2 rounded transition-all flex items-center space-x-2 ">
+            <Briefcase className='text-2xl text-green-600 dark:text-green-500' />
+            <span className="hidden sm:block">Our Services</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact"className="p-2 rounded transition-all flex items-center space-x-2">
+            <Contact className='text-2xl text-green-600 dark:text-green-500' />
+            <span className="hidden sm:block">Contact us</span>
+          </Link>
+        </li>
+
+    </ul>
+      </div>
           <ModeToggle/>
        </div>
     </div>
