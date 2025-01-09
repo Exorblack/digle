474e8708caaf35c4e63d118b090c11a88ef694cd
Mohamed Hassan <exorblack@gmail.com>
2025-01-09 16:32:05 +0200

    loading.

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -2,7 +2,6 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
-import Link from "next/link";
 
 interface works{
   id:string
